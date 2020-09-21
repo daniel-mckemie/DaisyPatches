375f386ac15288b024f77261978b68b1ff3c2357
Daniel McKemie <daniel.mckemie@gmail.com>
2020-09-21 04:17:31 -0400

    rough draft

--- ChaosGenerator/ChaosGenerator.cpp
+++ ChaosGenerator/ChaosGenerator.cpp
@@ -1,5 +1,7 @@
 #include "daisy_field.h"
 #include "daisysp.h" // Uncomment this if you want to use the DSP library.
+#include <string>
+
 
 using namespace daisysp;
 using namespace daisy;
@@ -8,18 +10,23 @@ using namespace daisy;
 // Declare a local daisy_field for hardware access
 DaisyField        hw;
 Comb              flt;
+Biquad            flt_2;
 static WhiteNoise noise;
-static Bitcrush   bitcrush;
+Oscillator        osc;
+// static Bitcrush   bitcrush;
 static Metro      metro;
 
 
 float kvals[8];
-float buf[9600];
+float buf[2400];
 uint8_t depth;
+
+
 // This runs at a fixed rate, to prepare audio samples
 static void callback(float *in, float *out, size_t size)
 {
-    hw.ProcessAnalogControls();    
+    hw.ProcessAnalogControls();
+    hw.UpdateDigitalControls();
 
     for(int i = 0; i < 8; i++)
     {
@@ -34,17 +41,32 @@ static void callback(float *in, float *out, size_t size)
 
     float sig1;
     float sig2;
+    float filter_sig;
     for(size_t i = 0; i < size; i += 2)
     {
-        sig1 = noise.Process();
-        flt.SetFreq(hw.GetKnobValue(0) * 4095);
-        flt.SetPeriod(hw.GetKnobValue(1));
+        sig1 = in[0];
+        filter_sig = osc.Process();
+        flt.SetFreq(hw.GetKnobValue(0) * 100);
+        flt.SetPeriod(hw.GetKnobValue(1) + 0.001);
         flt.SetRevTime(hw.GetKnobValue(2) * 5);
-        bitcrush.SetBitDepth((hw.GetKnobValue(3) * 1.0f) * depth + 2);
-        bitcrush.SetCrushRate((hw.GetKnobValue(4) + 2) * 2000);
-        sig2 = 0.5 * bitcrush.Process(sig2);
+        
 
-        sig1 = 0.5 * flt.Process(sig1);
+        // bitcrush.SetBitDepth((hw.GetKnobValue(3) * 1.0f) * depth + 2);
+        // bitcrush.SetCrushRate((hw.GetKnobValue(4) + 2) * 2000);
+
+
+        osc.SetFreq(hw.GetKnobValue(3) * 2000);
+        osc.SetAmp(hw.GetKnobValue(4));
+
+        flt_2.SetCutoff(hw.GetKnobValue(5) * 2000);
+        flt_2.SetRes(hw.GetKnobValue(6));
+
+
+        sig1 = flt.Process(sig1);
+    
+        sig2 = hw.GetKnobValue(7) * flt_2.Process(filter_sig);
+
+    
 
         // left out
         out[i] = (sig1 + sig2);
@@ -55,7 +77,6 @@ static void callback(float *in, float *out, size_t size)
 }
 
 
-
 int main(void)
 {
     float sample_rate;
@@ -64,34 +85,67 @@ int main(void)
 
     hw.Init();
     sample_rate = hw.SampleRate();
+    // uint32_t last_led_update, led_period, now;
 
-    for(int i = 0; i < 9600; i++)
+    // led_period = 5;
+    // last_led_update = now = dsy_system_getnow();
+
+
+    for(int i = 0; i < 2400; i++)
     {
         buf[i] = 0.0f;
     }
 
     // initialize Comb object
-    flt.Init(sample_rate, buf, 9600);
+    flt.Init(sample_rate, buf, 2400);
     flt.SetFreq(500);
     flt.SetPeriod(0.1);
     flt.SetRevTime(0.1);
 
     //initialize noise
     noise.Init();
-    bitcrush.Init(sample_rate);
+    // bitcrush.Init(sample_rate);
+    osc.Init(sample_rate);
+    flt_2.Init(sample_rate);    
     metro.Init(1.0f, sample_rate);
 
-    //set parameters for bitcrusher
-    bitcrush.SetBitDepth(6);
-    bitcrush.SetCrushRate(10000);
+    // //set parameters for bitcrusher
+    // bitcrush.SetBitDepth(6);
+    // bitcrush.SetCrushRate(10000);
+
+    osc.SetFreq(220);
+    osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
+    osc.SetAmp(0.5);
+    flt_2.SetRes(0.7);
+    
 
 
     hw.StartAudio(callback);
     hw.StartAdc();
 
+    // extern FontDef Font_16x26;
 
     while(1)
     {
-              
+        // now = dsy_system_getnow();
+
+        // if(now - last_led_update > led_period)
+        // {
+        //     // this was ugly, but the only way I could find to avoid
+        //     // getting yelled about defining note_names above
+
+            
+        //     // sprintf(strbuff, "Testing");
+
+        //     hw.display.Fill(false);
+        //     hw.display.SetCursor(0, 0);
+        //     // hw.display.WriteString(strbuff, Font_16x26, true);
+            
+        //     // RedrawRectangles(kvals);
+        //     char* knob_string = (char*)kvals;
+        //     hw.display.WriteString(knob_string, Font_16x26, true);
+        //     hw.display.Update();
+        //     // hw.display.Update();
+        // }
     }
 }
---
+++ ChaosGenerator/build/ChaosGenerator.d
@@ -0,0 +1,195 @@
+build/ChaosGenerator.o: ChaosGenerator.cpp \
+ ../../libdaisy/src/daisy_field.h ../../libdaisy/src/daisy_seed.h \
+ ../../libdaisy/src/daisy.h ../../libdaisy/src/daisy_core.h \
+ ../../libdaisy/src/sys/system.h ../../libdaisy/src/per/qspi.h \
+ ../../libdaisy/src/per/dac.h ../../libdaisy/src/per/gpio.h \
+ ../../libdaisy/src/per/sai.h ../../libdaisy/src/per/tim.h \
+ ../../libdaisy/src/dev/leddriver.h ../../libdaisy/src/per/i2c.h \
+ ../../libdaisy/src/dev/sdram.h ../../libdaisy/src/dev/sr_4021.h \
+ ../../libdaisy/src/hid/audio.h ../../libdaisy/src/per/sai.h \
+ ../../libdaisy/src/util/unique_id.h ../../libdaisy/src/per/adc.h \
+ ../../libdaisy/src/per/uart.h ../../libdaisy/src/hid/midi.h \
+ ../../libdaisy/src/util/ringbuffer.h ../../libdaisy/src/hid/encoder.h \
+ ../../libdaisy/src/hid/switch.h ../../libdaisy/src/hid/ctrl.h \
+ ../../libdaisy/src/hid/gatein.h ../../libdaisy/src/hid/parameter.h \
+ ../../libdaisy/src/hid/usb.h ../../libdaisy/src/per/sdmmc.h \
+ ../../libdaisy/src/per/spi.h ../../libdaisy/src/hid/oled_display.h \
+ ../../libdaisy/src/util/oled_fonts.h ../../libdaisy/src/hid/wavplayer.h \
+ ../../libdaisy/src/util/wav_format.h ../../libdaisy/src/hid/led.h \
+ ../../libdaisy/src/hid/rgb_led.h ../../libdaisy/src/util/color.h \
+ ../../libdaisy/src/dev/sr_595.h \
+ ../../libdaisy/src/util/scopedirqblocker.h \
+ ../../libdaisy/Drivers/CMSIS/Include/cmsis_gcc.h ../../DaisySP/daisysp.h \
+ ../../DaisySP/modules/adenv.h ../../DaisySP/modules/adsr.h \
+ ../../DaisySP/modules/allpass.h ../../DaisySP/modules/atone.h \
+ ../../DaisySP/modules/autowah.h ../../DaisySP/modules/balance.h \
+ ../../DaisySP/modules/biquad.h ../../DaisySP/modules/blosc.h \
+ ../../DaisySP/modules/comb.h ../../DaisySP/modules/bitcrush.h \
+ ../../DaisySP/modules/compressor.h ../../DaisySP/modules/dsp.h \
+ ../../DaisySP/modules/crossfade.h ../../DaisySP/modules/dcblock.h \
+ ../../DaisySP/modules/decimator.h ../../DaisySP/modules/delayline.h \
+ ../../DaisySP/modules/drip.h ../../DaisySP/modules/fold.h \
+ ../../DaisySP/modules/jitter.h ../../DaisySP/modules/limiter.h \
+ ../../DaisySP/modules/line.h ../../DaisySP/modules/maytrig.h \
+ ../../DaisySP/modules/metro.h ../../DaisySP/modules/mode.h \
+ ../../DaisySP/modules/moogladder.h ../../DaisySP/modules/nlfilt.h \
+ ../../DaisySP/modules/oscillator.h ../../DaisySP/modules/phasor.h \
+ ../../DaisySP/modules/port.h ../../DaisySP/modules/pitchshifter.h \
+ ../../DaisySP/modules/PolyPluck.h ../../DaisySP/modules/pluck.h \
+ ../../DaisySP/modules/reverbsc.h ../../DaisySP/modules/samplehold.h \
+ ../../DaisySP/modules/svf.h ../../DaisySP/modules/tone.h \
+ ../../DaisySP/modules/whitenoise.h
+
+../../libdaisy/src/daisy_field.h:
+
+../../libdaisy/src/daisy_seed.h:
+
+../../libdaisy/src/daisy.h:
+
+../../libdaisy/src/daisy_core.h:
+
+../../libdaisy/src/sys/system.h:
+
+../../libdaisy/src/per/qspi.h:
+
+../../libdaisy/src/per/dac.h:
+
+../../libdaisy/src/per/gpio.h:
+
+../../libdaisy/src/per/sai.h:
+
+../../libdaisy/src/per/tim.h:
+
+../../libdaisy/src/dev/leddriver.h:
+
+../../libdaisy/src/per/i2c.h:
+
+../../libdaisy/src/dev/sdram.h:
+
+../../libdaisy/src/dev/sr_4021.h:
+
+../../libdaisy/src/hid/audio.h:
+
+../../libdaisy/src/per/sai.h:
+
+../../libdaisy/src/util/unique_id.h:
+
+../../libdaisy/src/per/adc.h:
+
+../../libdaisy/src/per/uart.h:
+
+../../libdaisy/src/hid/midi.h:
+
+../../libdaisy/src/util/ringbuffer.h:
+
+../../libdaisy/src/hid/encoder.h:
+
+../../libdaisy/src/hid/switch.h:
+
+../../libdaisy/src/hid/ctrl.h:
+
+../../libdaisy/src/hid/gatein.h:
+
+../../libdaisy/src/hid/parameter.h:
+
+../../libdaisy/src/hid/usb.h:
+
+../../libdaisy/src/per/sdmmc.h:
+
+../../libdaisy/src/per/spi.h:
+
+../../libdaisy/src/hid/oled_display.h:
+
+../../libdaisy/src/util/oled_fonts.h:
+
+../../libdaisy/src/hid/wavplayer.h:
+
+../../libdaisy/src/util/wav_format.h:
+
+../../libdaisy/src/hid/led.h:
+
+../../libdaisy/src/hid/rgb_led.h:
+
+../../libdaisy/src/util/color.h:
+
+../../libdaisy/src/dev/sr_595.h:
+
+../../libdaisy/src/util/scopedirqblocker.h:
+
+../../libdaisy/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../../DaisySP/daisysp.h:
+
+../../DaisySP/modules/adenv.h:
+
+../../DaisySP/modules/adsr.h:
+
+../../DaisySP/modules/allpass.h:
+
+../../DaisySP/modules/atone.h:
+
+../../DaisySP/modules/autowah.h:
+
+../../DaisySP/modules/balance.h:
+
+../../DaisySP/modules/biquad.h:
+
+../../DaisySP/modules/blosc.h:
+
+../../DaisySP/modules/comb.h:
+
+../../DaisySP/modules/bitcrush.h:
+
+../../DaisySP/modules/compressor.h:
+
+../../DaisySP/modules/dsp.h:
+
+../../DaisySP/modules/crossfade.h:
+
+../../DaisySP/modules/dcblock.h:
+
+../../DaisySP/modules/decimator.h:
+
+../../DaisySP/modules/delayline.h:
+
+../../DaisySP/modules/drip.h:
+
+../../DaisySP/modules/fold.h:
+
+../../DaisySP/modules/jitter.h:
+
+../../DaisySP/modules/limiter.h:
+
+../../DaisySP/modules/line.h:
+
+../../DaisySP/modules/maytrig.h:
+
+../../DaisySP/modules/metro.h:
+
+../../DaisySP/modules/mode.h:
+
+../../DaisySP/modules/moogladder.h:
+
+../../DaisySP/modules/nlfilt.h:
+
+../../DaisySP/modules/oscillator.h:
+
+../../DaisySP/modules/phasor.h:
+
+../../DaisySP/modules/port.h:
+
+../../DaisySP/modules/pitchshifter.h:
+
+../../DaisySP/modules/PolyPluck.h:
+
+../../DaisySP/modules/pluck.h:
+
+../../DaisySP/modules/reverbsc.h:
+
+../../DaisySP/modules/samplehold.h:
+
+../../DaisySP/modules/svf.h:
+
+../../DaisySP/modules/tone.h:
+
+../../DaisySP/modules/whitenoise.h:
