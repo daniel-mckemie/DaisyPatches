26f33c93c1bc7118f44364a1ea3950c0de61a348
Daniel McKemie <daniel.mckemie@gmail.com>
2020-09-23 01:59:01 -0400

    pm built, not debugged

--- ChaosGenerator/ChaosGenerator.cpp
+++ ChaosGenerator/ChaosGenerator.cpp
@@ -12,7 +12,9 @@ DaisyField        hw;
 Comb              flt;
 Biquad            flt_2;
 static WhiteNoise noise;
-Oscillator        osc;
+Oscillator        osc_1;
+Oscillator        osc_2;
+ReverbSc          verb;
 // static Bitcrush   bitcrush;
 static Metro      metro;
 
@@ -28,6 +30,17 @@ static void callback(float *in, float *out, size_t size)
     hw.ProcessAnalogControls();
     hw.UpdateDigitalControls();
 
+    if(hw.KeyboardState(0))
+    {
+        osc_1.SetWaveform(Oscillator::WAVE_SAW);
+        osc_2.SetWaveform(Oscillator::WAVE_SAW);
+    }
+    if(hw.KeyboardState(1))
+    {
+        osc_1.SetWaveform(Oscillator::WAVE_SQUARE);
+        osc_2.SetWaveform(Oscillator::WAVE_SQUARE);
+    }
+
     for(int i = 0; i < 8; i++)
     {
         kvals[i] = hw.GetKnobValue(i);
@@ -41,32 +54,34 @@ static void callback(float *in, float *out, size_t size)
 
     float sig1;
     float sig2;
+    float sig3;
     float filter_sig;
     for(size_t i = 0; i < size; i += 2)
     {
         sig1 = in[0];
-        filter_sig = osc.Process();
-        flt.SetFreq(hw.GetKnobValue(0) * 100);
+        sig2 = osc_1.Process();
+        sig3 = osc_2.Process();
+        flt.SetFreq(hw.GetKnobValue(0) * 2000);
         flt.SetPeriod(hw.GetKnobValue(1) + 0.001);
-        flt.SetRevTime(hw.GetKnobValue(2) * 5);
+        flt.SetRevTime(hw.GetKnobValue(2) * 4);
+        
+        
+        osc_1.SetFreq(hw.GetKnobValue(3) * 2000);
+
+        // osc_2.Process(); 
+        osc_2.SetFreq(hw.GetKnobValue(3) * 2000);
+        osc_2.PhaseAdd(hw.GetKnobValue(4) / 500);
+        osc_2.SetAmp(hw.GetKnobValue(5));
         
 
-        // bitcrush.SetBitDepth((hw.GetKnobValue(3) * 1.0f) * depth + 2);
-        // bitcrush.SetCrushRate((hw.GetKnobValue(4) + 2) * 2000);
+        sig1 = flt.Process(sig1);        
 
+        // sig2 = hw.GetKnobValue(7) * flt_2.Process(filter_sig);
+        sig2 = (hw.GetKnobValue(6) * sig2) + sig3;
+        // verb.Process(sig2, sig2, &out[i], &out[i + 1]);
+        // verb.SetFeedback(hw.GetKnobValue(6) * 22499);
+        sig1 = flt.Process(sig2);
 
-        osc.SetFreq(hw.GetKnobValue(3) * 2000);
-        osc.SetAmp(hw.GetKnobValue(4));
-
-        flt_2.SetCutoff(hw.GetKnobValue(5) * 2000);
-        flt_2.SetRes(hw.GetKnobValue(6));
-
-
-        sig1 = flt.Process(sig1);
-    
-        sig2 = hw.GetKnobValue(7) * flt_2.Process(filter_sig);
-
-    
 
         // left out
         out[i] = (sig1 + sig2);
@@ -100,12 +115,13 @@ int main(void)
     flt.Init(sample_rate, buf, 2400);
     flt.SetFreq(500);
     flt.SetPeriod(0.1);
-    flt.SetRevTime(0.1);
+    flt.SetRevTime(0.385);
 
     //initialize noise
     noise.Init();
     // bitcrush.Init(sample_rate);
-    osc.Init(sample_rate);
+    osc_1.Init(sample_rate);
+    osc_2.Init(sample_rate);
     flt_2.Init(sample_rate);    
     metro.Init(1.0f, sample_rate);
 
@@ -113,11 +129,14 @@ int main(void)
     // bitcrush.SetBitDepth(6);
     // bitcrush.SetCrushRate(10000);
 
-    osc.SetFreq(220);
-    osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
-    osc.SetAmp(0.5);
+    osc_1.SetFreq(220);
+    osc_1.SetWaveform(Oscillator::WAVE_SAW);
+    osc_1.SetAmp(1);
+    osc_2.SetFreq(220);
+    osc_2.SetWaveform(Oscillator::WAVE_SAW);
+    osc_2.SetAmp(1);
+
     flt_2.SetRes(0.7);
-    
 
 
     hw.StartAudio(callback);
